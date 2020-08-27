41de0ecc2a652a869823a5a001a2a47e29af51e9
linhsol26 <nhglinh.sol@gmail.com>
2020-08-27 15:11:59 +0700

    uninstall primeng & add A-material

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UIModule } from './ui/ui.module';
 import { HomeModule } from './home/home.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HomeModule } from './home/home.module';
     BrowserModule,
     AppRoutingModule,
     UIModule,
-    HomeModule
+    HomeModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
