76e6968f5b050879ebbae86b1baeea2e690dbaba
LeKimThoa <lekimthoa0204@gmail.com>
2020-09-01 17:53:35 +0700

    Init

--- front-end/src/app/upload/upload.module.ts
+++ front-end/src/app/upload/upload.module.ts
@@ -5,10 +5,12 @@ import { UploadRoutingModule } from './upload-routing.module';
 import { UploadComponent } from './upload.component';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import { UploaderComponent } from './uploader/uploader.component';
+import { UploadTaskComponent } from './upload-task/upload-task.component';
 
 
 @NgModule({
-  declarations: [UploadComponent],
+  declarations: [UploadComponent, UploaderComponent, UploadTaskComponent],
   imports: [
     CommonModule,
     UploadRoutingModule,
