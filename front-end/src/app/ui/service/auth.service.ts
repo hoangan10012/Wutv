d07ab92c9195745c1d2ca37325225a3184a8ae50
LeKimThoa <lekimthoa0204@gmail.com>
2020-09-04 17:04:36 +0700

    Wutv

--- front-end/src/app/ui/service/auth.service.ts
+++ front-end/src/app/ui/service/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import * as firebase from 'firebase';
+import {auth} from 'firebase';
 import { Router } from '@angular/router';
 import {AngularFireAuth} from '@angular/fire/auth';
 import {HttpClient} from '@angular/common/http';
@@ -11,16 +11,23 @@ import {User} from '../../models/user.model'
 export class AuthenticationService 
 {
   public user:firebase.User=null;
-  private usr:User;
+  logged = false;
   endpoint = "http://127.0.0.1";
   port = 8080;
-  constructor(public auth:AngularFireAuth, public router: Router, private httpClient:HttpClient ) { }
+  constructor(public _auth:AngularFireAuth, public router: Router, private httpClient:HttpClient ) { 
+    this._auth.user.subscribe(usr => {
+      if (usr != null) {
+        this.user = usr;
+        this.logged = true;
+      } else { this.logged = false; }
+    });
+  }
   public async login()
   {
     try{
-      let provider = new firebase.auth.GoogleAuthProvider();
-      await this.auth.signInWithPopup(provider);
-      this.user=await this.auth.currentUser;
+      let provider = new auth.GoogleAuthProvider();
+      await this._auth.signInWithPopup(provider);
+      this.user = await this._auth.currentUser;
       await this.httpClient.post(this.endpoint+this.port+'/v1/User/Post', {
         id: this.user.uid,
         type: "",
@@ -32,14 +39,14 @@ export class AuthenticationService
         likes : [],
         dislikes: []
       }).toPromise();
-      this.router.navigate(["/home"]);
+      this.router.navigate(['home']);
   
     }catch(err){
         console.log(err)
     }
   }
   public async logout (){
-    await this.auth.signOut;
+    await this._auth.signOut();
     this.user = null;
   }
   
