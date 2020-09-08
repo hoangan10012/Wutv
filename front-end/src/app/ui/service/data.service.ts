1868af8b987afd12c51dfdb80a905f8094c59722
LeKimThoa <lekimthoa0204@gmail.com>
2020-09-08 10:52:07 +0700

    check again

--- front-end/src/app/ui/service/data.service.ts
+++ front-end/src/app/ui/service/data.service.ts
@@ -4,12 +4,13 @@ import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs';
 import { data } from '../../models/data.model';
+import * as firebase from 'firebase';
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor(private fb: AngularFirestore, private ht: HttpClient) { }
+  constructor(private fb: AngularFirestore, private ht: HttpClient, private _afs: AngularFirestore) { }
 
   public async getvideo() {
     let videos = Array<data>();
@@ -24,7 +25,65 @@ export class DataService {
 
   }
 
+  addLike( vid, uid) {
+    this._afs
+    .collection('videos')
+    .doc(vid)
+    .update({
+      likes : firebase.firestore.FieldValue.arrayUnion(uid)
+    });
+    this._afs
+    .collection('users')
+    .doc(uid)
+    .update({
+      likes: firebase.firestore.FieldValue.arrayUnion(vid)
+    });
+  }
 
+  addDislike( vid, uid) {
+    this._afs
+    .collection('videos')
+    .doc(vid)
+    .update({
+      dislikes : firebase.firestore.FieldValue.arrayUnion(uid)
+    });
+    this._afs
+    .collection('users')
+    .doc(uid)
+    .update({
+      dislikes : firebase.firestore.FieldValue.arrayUnion(vid)
+    });
+  }
+
+  removeLike(vid, uid) {
+    this._afs
+    .collection('videos')
+    .doc(vid)
+    .update({
+      likes : firebase.firestore.FieldValue.arrayRemove(uid)
+    });
+    this._afs
+    .collection('users')
+    .doc(uid)
+    .update({
+      likes : firebase.firestore.FieldValue.arrayRemove(vid)
+    });
+  }
+
+  removeDislike(vid, uid) {
+    this._afs
+    .collection('videos')
+    .doc(vid)
+    .update({
+      dislikes : firebase.firestore.FieldValue.arrayRemove(uid)
+    });
+    this._afs
+    .collection('users')
+    .doc(uid)
+    .update({
+      dislikes : firebase.firestore.FieldValue.arrayRemove(vid)
+    });
+  }
 
 
 }
